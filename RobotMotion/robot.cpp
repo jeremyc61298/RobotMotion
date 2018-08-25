8b2e5e538f27c9b15df278e966ebd70dc2db3e98
Jeremy Campbell <jcampbell2@harding.edu>
2018-08-25 14:20:48 -0500

    Project has passed all supplied test cases. A robot will enter the grid, and find its way out. If it gets stuck in a loop, it will report how many steps are in the loop, and how many were before the loop.

--- RobotMotion/robot.cpp
+++ RobotMotion/robot.cpp
@@ -1,3 +1,6 @@
+// Robot Motion - robot.cpp
+// Jeremy Campbell
+
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -13,15 +16,15 @@ using std::string;
 
 struct Point
 {
-	int x = 0;
 	int y = 0;
+	int x = 0;
 };
 
 struct GridPoint
 {
 	char directionToMove;
 	int instructionCount;
-	bool visited = false;
+	bool visited;
 };
 
 struct RobotGrid
@@ -49,9 +52,38 @@ void initializeGrid(RobotGrid &grid, ifstream &fin)
 		for (int j = 0; j < grid.cols; j++)
 		{
 			grid.map[i][j].directionToMove = line[j];
+			grid.map[i][j].instructionCount = 0;
+			grid.map[i][j].visited = false;
 		}
 	}
 
+	// Erase remaining data from previous grids
+	grid.loopSize = 0;
+	grid.numOfInstructions = 0;
+}
+
+bool moveRobot(Point &robot, char directionToMove)
+{
+	bool result = true;
+	switch (directionToMove)
+	{
+	case 'N':
+		--robot.y;
+		break;
+	case 'E':
+		++robot.x;
+		break;
+	case 'S':
+		++robot.y;
+		break;
+	case 'W':
+		--robot.x;
+		break;
+	default:
+		cout << "Invalid direction. ";
+		result = false;
+	}
+	return result;
 }
 
 void traverseGrid(RobotGrid &grid)
@@ -59,19 +91,73 @@ void traverseGrid(RobotGrid &grid)
 	Point robot;
 	bool finished = false;
 	
-	// Initialize the start point of the robot
+	// Initialize the start point of the robot and make first move
 	robot.x = grid.x;
 	robot.y = grid.y;
+	grid.map[robot.y][robot.x].visited = true;
+	if (!moveRobot(robot, grid.map[robot.y][robot.x].directionToMove))
+	{
+		finished = true;
+		grid.loopSize = -1;
+		cout << "Grid information: " << grid.rows << grid.cols << grid.y << grid.x << endl;
+	}
 
+	// Continue to traverse the grid
 	while (!finished)
 	{
-		if (robot.x < 0 || robot.x > grid.rows || robot.y < 0 || robot.y > grid.cols)
+		++grid.numOfInstructions;
+		if (robot.y < 0 || robot.y > grid.rows - 1 || robot.x < 0 || robot.x > grid.cols - 1)
 		{
 			finished = true;
 		}
-		else if (grid.map[robot.x][robot.y].visited)
+		else if (grid.map[robot.y][robot.x].visited)
 		{
-			grid.loopSize = grid.numOfInstructions - grid.map[robot.x][robot.y].instructionCount;
+			grid.loopSize = grid.numOfInstructions - grid.map[robot.y][robot.x].instructionCount;
+			finished = true;
+		}
+		else
+		{
+			// Breadcrumb the spot, then attempt to move. If moving fails, write an error message
+			// and mark the grid as failed with a -1.
+			grid.map[robot.y][robot.x].visited = true;
+			grid.map[robot.y][robot.x].instructionCount = grid.numOfInstructions;
+
+			if (!moveRobot(robot, grid.map[robot.y][robot.x].directionToMove))
+			{
+				finished = true; 
+				grid.loopSize = -1;
+				cout << "Grid information: " << grid.rows << grid.cols << grid.y << grid.x << endl;
+			}
+		}
+	}
+}
+
+void displayResults(RobotGrid &grid, ofstream &fout)
+{
+	if (grid.loopSize == 0)
+	{
+		fout << grid.numOfInstructions << " instruction";
+		if (grid.numOfInstructions != 1)
+		{
+			fout << "s";
+		}
+		fout << " before exit";
+	}
+	else if (grid.loopSize == -1)
+	{
+		fout << "Error moving the robot.";
+	}
+	else 
+	{
+		fout << grid.numOfInstructions - grid.loopSize << " instruction";
+		if (grid.numOfInstructions - grid.loopSize != 1)
+		{
+			fout << "s";
+		}
+		fout << " before a loop of " << grid.loopSize << " instruction";
+		if (grid.loopSize != 1)
+		{
+			fout << "s";
 		}
 	}
 }
@@ -88,16 +174,23 @@ int main()
 	
 	RobotGrid grid;
 
-	fin >> grid.rows >> grid.cols >> grid.x >> grid.y;
+	fin >> grid.rows >> grid.cols >> grid.y >> grid.x;
 	fin.ignore();
 
 	while (grid.rows != 0 && grid.cols != 0 && grid.x != 0 && grid.y != 0)
 	{
+		// Format to a zero base array
+		--grid.y;
+		--grid.x;
 		initializeGrid(grid, fin);
 		traverseGrid(grid);
+		displayResults(grid, fout);
 		// Read in the next grid information
-		fin >> grid.rows >> grid.cols >> grid.x >> grid.y;
+		fin >> grid.rows >> grid.cols >> grid.y >> grid.x;
+		if (grid.rows != 0 && grid.cols != 0 && grid.x != 0 && grid.y != 0)
+		{
+			fout << endl;
+		}
 		fin.ignore();
 	}
-
 }

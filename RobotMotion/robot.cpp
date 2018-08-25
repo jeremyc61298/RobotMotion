baecd972727ef0cf52f1286f97e734540d81ff64
Jeremy Campbell <jcampbell2@harding.edu>
2018-08-24 22:23:43 -0500

    Can now init Grid structs.

--- RobotMotion/robot.cpp
+++ RobotMotion/robot.cpp
@@ -1,35 +1,73 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
+using std::vector;
+using std::string;
 
 struct GridPoint
 {
 	char directionToMove;
 	int instructionCount;
-	bool visited;
+	bool visited = false;
 };
+
+struct RobotGrid
+{
+	vector<vector<GridPoint>> map;
+	int rows, cols, x, y;
+};
+
+void initializeGrid(RobotGrid &grid, ifstream &fin)
+{
+	// Resize grid
+	grid.map.resize(grid.rows);
+	for (int i = 0; i < grid.map.size(); i++)
+	{
+		grid.map[i].resize(grid.cols);
+	}
+	
+	// Fill Grid
+	for (int i = 0; i < grid.rows; i++)
+	{
+		string line;
+		getline(fin, line);
+		for (int j = 0; j < grid.cols; j++)
+		{
+			grid.map[i][j].directionToMove = line[j];
+		}
+	}
+
+}
 
 int main()
 {
 	ifstream fin("robot.in");
-	if (!fin.is_open)
+	if (!fin.is_open())
 	{
 		return -1;
 	}
 
 	ofstream fout("robot.out");
-	int rows, cols, x, y;
+	
+	RobotGrid grid;
 
-	fin >> rows >> cols >> x >> y;
+	fin >> grid.rows >> grid.cols >> grid.x >> grid.y;
+	fin.ignore();
 
-	while (rows != 0 && cols != 0 && x != 0 && y != 0)
+	while (grid.rows != 0 && grid.cols != 0 && grid.x != 0 && grid.y != 0)
 	{
+		initializeGrid(grid, fin);
 
+		// Read in the next grid information
+		fin >> grid.rows >> grid.cols >> grid.x >> grid.y;
+		fin.ignore();
 	}
 
 }
